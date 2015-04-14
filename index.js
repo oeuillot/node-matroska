12f070121bc631a82336724a6737e3f85afbb095
oeuillot <olivier.oeuillot@gmail.com>
2015-04-14 17:28:48 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*jslint node: true, vars: true, nomen: true */
+'use strict';
+
+module.exports = {
+	Decoder: require('./lib/decoder'),
+	Document: require('./lib/document'),
+	Document2: require('./lib/document2'),
+	Tag: require('./lib/tag')
+};
