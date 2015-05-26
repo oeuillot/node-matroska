5cdf6531e40ed130f027a54173cee2a1ee5d0429
oeuillot <olivier.oeuillot@gmail.com>
2015-05-26 23:26:21 +0200

    Fixes

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var ebml = require('./index.js');
 var fs = require('fs');
 var util = require('util');
 
-var decoder = new ebml.Decoder(EbmlDecoder.OnlyMetaDatas());
+var decoder = new ebml.Decoder(ebml.Decoder.OnlyMetaDatas());
 
 var filename = process.argv[2];
 console.log("Parsing ... " + filename);
