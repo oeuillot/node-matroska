e329a49b759352b637972286540e09bd8f7748aa
oeuillot <olivier.oeuillot@gmail.com>
2015-05-26 23:20:37 +0200

    Add example

---
+++ example.js
@@ -0,0 +1,18 @@
+var ebml = require('./index.js');
+var fs = require('fs');
+var util = require('util');
+
+var decoder = new ebml.Decoder();
+
+var filename = process.argv[2];
+console.log("Parsing ... " + filename);
+
+var t1 = Date.now();
+
+decoder.parseFileInfoTagsAndAttachments(filename, function(error, document) {
+	var t2 = Date.now();
+
+	console.log("Delta=" + (t2 - t1) + "ms");
+
+	console.log(document.print());
+});
