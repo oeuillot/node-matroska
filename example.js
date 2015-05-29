484571c5a790fa9f7d4aa9d095597667fe83d710
oeuillot <olivier.oeuillot@gmail.com>
2015-05-29 15:55:47 +0200

    Use source get load content (and not only a file) Add new source types  (file support, http support, streamFactory)

--- example.js
+++ example.js
@@ -1,18 +1,28 @@
-var ebml = require('./index.js');
+var matroska = require('./index.js');
 var fs = require('fs');
 var util = require('util');
 
-var decoder = new ebml.Decoder(ebml.Decoder.OnlyMetaDatas());
+var decoder = new matroska.Decoder(matroska.Decoder.OnlyMetaDatas());
 
 var filename = process.argv[2];
+
+if (!filename) {
+	filename = "http://download.wavetlan.com/SVV/Media/HTTP/mkv/H264_PCM(mkvmerge).mkv";
+}
+
 console.log("Parsing ... " + filename);
 
 var t1 = Date.now();
 
-decoder.parseFileInfoTagsAndAttachments(filename, function(error, document) {
+decoder.parseInfoTagsAndAttachments(filename, function(error, document) {
 	var t2 = Date.now();
 
 	console.log("Delta=" + (t2 - t1) + "ms");
+
+	if (error) {
+		console.error(error);
+		return;
+	}
 
 	console.log(document.print());
 });
--- index.js
+++ index.js
@@ -4,5 +4,8 @@
 module.exports = {
 	Decoder: require('./lib/decoder'),
 	Document: require('./lib/document'),
-	Schema: require('./lib/schema')
+	Schema: require('./lib/schema'),
+	FileSource: require('./lib/source/fileSource'),
+	HttpSource: require('./lib/source/httpSource'),
+	StreamFactorySource: require('./lib/source/streamFactorySource')
 };
